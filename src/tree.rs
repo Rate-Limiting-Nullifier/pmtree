c708ea9fda4af5bd4fcca8335795526059d4c6de
Magamedrasul Ibragimov <curryrasul@gmail.com>
2022-10-28 13:25:56 +0300

    feat: add new functions for Merkle Tree struct

--- src/tree.rs
+++ src/tree.rs
@@ -7,7 +7,8 @@ where
 {
     db: D,
     h: H,
-    depth: u8,
+    depth: usize,
+    next_index: usize,
 }
 
 impl<D, H> MerkleTree<D, H>
@@ -15,11 +16,43 @@ where
     D: Database,
     H: Hasher,
 {
-    pub fn new(depth: u8, dbpath: &str) -> Self {
+    /// Creates new `MerkleTree`
+    pub fn new(depth: usize, dbpath: &str) -> Self {
         Self {
             db: Database::new(dbpath),
             h: Hasher::new(),
             depth,
+            next_index: 0,
         }
+    }
+
+    /// Inserts a leaf to the next available index
+    pub fn insert(&mut self, leaf: H::Fr) {
+        todo!()
+    }
+
+    /// Deletes a  leaf at the `key` by setting it to its default value
+    pub fn delete(&mut self, key: usize) {
+        todo!()
+    }
+
+    /// Returns the root of the tree
+    pub fn root(&self) -> H::Fr {
+        todo!()
+    }
+
+    /// Returns the total number of leaves set
+    pub fn leaves_set(&self) -> usize {
+        self.next_index
+    }
+
+    /// Returns the capacity of the tree, i.e. the maximum number of leaves
+    pub fn capacity(&self) -> usize {
+        1 << self.depth
+    }
+
+    /// Returns the depth of the tree
+    pub fn depth(&self) -> usize {
+        self.depth
     }
 }
